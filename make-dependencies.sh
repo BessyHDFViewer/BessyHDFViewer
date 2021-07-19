0a433002612f94706dd1d64d0e3a3b551223eaa6
Christian daheim <auriocus@gmx.de>
2021-07-19 17:04:16 +0200

    Provide an alias for Windows runtime (fix CI)

--- make-dependencies.sh
+++ make-dependencies.sh
@@ -24,6 +24,17 @@ mkdir -p "$rtdir"
 download https://github.com/BessyHDFViewer/HDFpp/releases/download/latest/HDFpp_$machine.tar.bz2 dependencies/HDFpp_$machine.tar.bz2
 download https://github.com/auriocus/kbskit/releases/download/latest/kbskit_$machine.tar.bz2 dependencies/kbskit_$machine.tar.bz2
 
+# in case of Windows, create an alias from the complex MINGW-name
+if [[ "$machine" == *MINGW* ]]; then
+	smachine=Windows-x86_64
+	( cd dependencies
+		
+	  ln -s HDFpp_$machine.tar.bz2 HDFpp_$smachine.tar.bz2
+	  ln -s kbskit_$machine.tar.bz2 kbskit_$smachine.tar.bz2
+	  )
+fi
+
+
 # HDFpp is prepared for BessyHDFViewer. Simply extract 
 # into the libdir
 
